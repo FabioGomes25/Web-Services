262cda36defb1f31cb9d5381318c79b5a9bd4731
Stan Pak <uriasan1@gmail.com>
2014-01-29 09:54:00 -0800

    WCF Lab Assignment 1

---
+++ WebMathClient/WebMathClient/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace WebMathClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValueOne = new System.Windows.Forms.TextBox();
+            this.txtValueTwo = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnModulus = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtValueOne
+            // 
+            this.txtValueOne.Location = new System.Drawing.Point(131, 100);
+            this.txtValueOne.Name = "txtValueOne";
+            this.txtValueOne.Size = new System.Drawing.Size(100, 20);
+            this.txtValueOne.TabIndex = 0;
+            // 
+            // txtValueTwo
+            // 
+            this.txtValueTwo.Location = new System.Drawing.Point(131, 165);
+            this.txtValueTwo.Name = "txtValueTwo";
+            this.txtValueTwo.Size = new System.Drawing.Size(100, 20);
+            this.txtValueTwo.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(331, 100);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Location = new System.Drawing.Point(331, 147);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(75, 23);
+            this.btnSubtract.TabIndex = 3;
+            this.btnSubtract.Text = "Subtract";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(331, 194);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiply.TabIndex = 4;
+            this.btnMultiply.Text = "Multiply";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(331, 242);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(75, 23);
+            this.btnDivide.TabIndex = 5;
+            this.btnDivide.Text = "Divide";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // btnModulus
+            // 
+            this.btnModulus.Location = new System.Drawing.Point(331, 288);
+            this.btnModulus.Name = "btnModulus";
+            this.btnModulus.Size = new System.Drawing.Size(75, 23);
+            this.btnModulus.TabIndex = 6;
+            this.btnModulus.Text = "Modulus";
+            this.btnModulus.UseVisualStyleBackColor = true;
+            this.btnModulus.Click += new System.EventHandler(this.btnModulus_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(137, 293);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Value 1: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Value 2: ";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(12, 272);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 54);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 359);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnModulus);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtValueTwo);
+            this.Controls.Add(this.txtValueOne);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValueOne;
+        private System.Windows.Forms.TextBox txtValueTwo;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnModulus;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ WebMathClient/WebMathClient/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WebMathClient
+{
+    public partial class Form1 : Form
+    {
+        MathService.CalculatorClient service = new MathService.CalculatorClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double v1 = Convert.ToDouble(txtValueOne.Text);
+                double v2 = Convert.ToDouble(txtValueTwo.Text);
+                lblResult.Text = service.Add(v1, v2);
+            }catch(Exception exc)
+            {
+                lblResult.Text = exc.Message;
+            }
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double v1 = Convert.ToDouble(txtValueOne.Text);
+                double v2 = Convert.ToDouble(txtValueTwo.Text);
+                lblResult.Text = service.Subtract(v1, v2);
+            }catch(Exception exc)
+            {
+                lblResult.Text = exc.Message;
+            }
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double v1 = Convert.ToDouble(txtValueOne.Text);
+                double v2 = Convert.ToDouble(txtValueTwo.Text);
+                lblResult.Text = service.Multiply(v1, v2);
+            }catch (Exception exc)
+            {
+                lblResult.Text = exc.Message;
+            }
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double v1 = Convert.ToDouble(txtValueOne.Text);
+                double v2 = Convert.ToDouble(txtValueTwo.Text);
+                lblResult.Text = service.Divide(v1, v2);
+            }catch (Exception exc)
+            {
+                lblResult.Text = exc.Message;
+            }
+        }
+
+        private void btnModulus_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double v1 = Convert.ToDouble(txtValueOne.Text);
+                double v2 = Convert.ToDouble(txtValueTwo.Text);
+                lblResult.Text = service.Modulus(v1, v2);
+            }catch (Exception exc)
+            {
+                lblResult.Text = exc.Message;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
